98b1eee5a5dcef1f908ba533569a7afc22ff4c59
Cp <yoshyparrott@gmail.com>
2016-05-12 11:08:29 +0200

    Cleaned up included bootstrap

--- Website/index.php
+++ Website/index.php
@@ -1,9 +1,6 @@
 <?php require 'header.php';?>
 <!-- HTML HERE -->
-<p>HTML</p>
-<div class='jumbotron'>
-  <h1>test</h1>
-  <p>test</p>
-</div>
+
+
 <!-- HTML HERE -->
 <?php require 'footer.php';?>
